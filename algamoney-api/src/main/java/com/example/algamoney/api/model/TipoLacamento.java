49aefcf6064e8b941a6f8566cdc38560ad3237f9
antonia <wix12@hotmail.com>
2018-08-09 22:57:22 -0300

    criando a migracao e entidade de lancamento

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/model/TipoLacamento.java
@@ -0,0 +1,8 @@
+package com.example.algamoney.api.model;
+
+public enum TipoLacamento {
+	
+	RECEITA,
+	DESPESA
+
+}
--- algamoney-api/src/main/resources/db/migration/V02__criar_e_resigistrar_pessoa.sql
+++ algamoney-api/src/main/resources/db/migration/V02__criar_e_resigistrar_pessoa.sql
@@ -11,3 +11,5 @@ ativo boolean NOT NULL)
 ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO pessoa (nome, logradouro, numero, bairro, cidade, estado, ativo) values ('Ana Maria', 'Rua Amazonas','56','Novo Horizonte','Marabá','Pará', true);
+INSERT INTO pessoa (nome, logradouro, numero, bairro, cidade, estado, ativo) values ('Paula Maria', 'Rua Amazonas','16','Novo Horizonte','Marabá','Pará', true);
+INSERT INTO pessoa (nome, logradouro, numero, bairro, cidade, estado, ativo) values ('Joao Maria', 'Rua Amazonas','5','Novo Horizonte','Marabá','Pará', true);
